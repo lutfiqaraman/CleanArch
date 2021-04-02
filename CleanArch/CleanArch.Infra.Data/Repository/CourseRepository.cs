d013d06357eba3b7ef95becb0d0126e36f5c480f
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-02 21:45:28 +0300

    Implement course repository

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        public UniversityDBContext Context { get; private set; }
+
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            Context = universityDBContext;
+        }
+
+        IEnumerable<Course> ICourseRepository.GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
