8caf144c5005dea3857184f1d31bf8c24188e465
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-02 23:38:07 +0300

    Install Microsoft.Extensions.DependencyInjection

--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -45,6 +45,7 @@ namespace CleanArch.MVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
