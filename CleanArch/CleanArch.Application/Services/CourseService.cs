c869c455aefa965d5945279cbf21b0b7056ecfa6
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-02 21:08:40 +0300

    Implement ICourseService

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        public ICourseRepository CourseRepository { get; private set; }
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            CourseRepository = courseRepository;
+        }
+
+        public IEnumerable<CourseViewModel> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
