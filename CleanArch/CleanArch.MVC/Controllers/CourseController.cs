bc97b03ebdf809a45a25c8e97a0e57e5da5a3c8c
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-03 10:03:51 +0300

    create course controller and view

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ICourseService CourseService { get; private set; }
+
+        public CourseController(ICourseService courseSrv)
+        {
+            CourseService = courseSrv;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = CourseService.GetCourses();
+            return View(model);
+        }
+    }
+}
