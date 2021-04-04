8ad18d9f4db34de6a7ba252b603923ed18d1e411
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-04 23:04:56 +0300

    Build CourseCommandHandler

---
+++ CleanArch/CleanArch.Domain/CommandHandlers/CourseCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CleanArch.Domain.Commands;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.CommandHandlers
+{
+    public class CourseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        public ICourseRepository CourseRepository { get; private set; }
+
+        public CourseCommandHandler(ICourseRepository courseRepository)
+        {
+            CourseRepository = courseRepository;
+        }
+
+        public Task<bool> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            Course course = new Course()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+
+            //CourseRepository.Add(course);
+
+            return Task.FromResult(true);
+        }
+    }
+}
