379f9689ec1aa12437887dd41a62140598d83f2f
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-08 00:10:55 +0300

    using IoC project

--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArch.Infra.IoC
@@ -10,8 +16,11 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection srv)
         {
+            srv.AddScoped<IMediatorHandler, InMemoryBus>();
+            srv.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
             srv.AddScoped<ICourseService, CourseService>();
             srv.AddScoped<ICourseRepository, CourseRepository>();
+            srv.AddScoped<UniversityDBContext>();
         }
     }
 }
