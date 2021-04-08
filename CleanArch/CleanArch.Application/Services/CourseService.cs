df4720076714b9e71a4f0ac7486b03389d7a72a0
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-08 19:27:42 +0300

    update service layer

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -12,10 +14,12 @@ namespace CleanArch.Application.Services
     public class CourseService : ICourseService
     {
         public ICourseRepository CourseRepository { get; private set; }
+        public IMediatorHandler Bus { get; private set; }
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             CourseRepository = courseRepository;
+            Bus = bus;
         }
 
         public CourseViewModel GetCourses()
@@ -27,6 +31,14 @@ namespace CleanArch.Application.Services
 
 
             return result;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            CreateCourseCommand createCourseCommand = 
+                new CreateCourseCommand(courseViewModel.Name, courseViewModel.Description, courseViewModel.ImageUrl);
+
+            Bus.SendCommand(createCourseCommand);
         }
     }
 }
